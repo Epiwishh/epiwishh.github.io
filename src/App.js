37350abb83c59f1b6db7c7847c2033dadc225965
Emre ÇELİK <emrecelikbu@gmail.com>
2022-04-08 21:06:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react'
 import './App.css';
+import igredientsData from './data/igredients.json'
+import recipesData from './data/recipes.json'
+import Recipe from './Recipe';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [ingredients, setIngredients] = useState([])
+  const [recipes, setRecipes] = useState([])
+  
+  
+  const addItem = (index) => {
+    const isItemExist = ingredients.includes(igredientsData[index].name);
+    if (isItemExist) {
+      const removeIndex = ingredients.indexOf(igredientsData[index].name) ;
+      ingredients.splice(removeIndex, 1);
+      setIngredients([...ingredients]);
+    } else {
+      setIngredients([...ingredients, igredientsData[index].name]);
+
+    }
+  }
+
+  const getRecipes = () => {
+    var newList = [];
+
+    if (ingredients.length > 0) {
+      
+      for (var x = 0; x < recipesData.length; x++) {
+          const recipeIgreditens = recipesData[x].ingredient;
+          const list = recipeIgreditens.map(item => item.name);
+
+
+          const hasAll = ingredients.every(value => {
+            return list.includes(value);
+          });
+
+          if (hasAll) {
+            newList.push(recipesData[x]);
+          }  
+
+        }
+
+      setRecipes(newList);
+
+    } else {
+      alert('Please select at least 1 ingredient');
+    }
+  }
+
+  useEffect(()=> {
+    console.log(ingredients);
+  }, [ingredients])
+
+  useEffect(()=> {
+    console.log(recipes);
+  }, [recipes])
+
+  function RecipePage() {
+    return (
+      <>
+      <br/>
+      <center><h2>Select Ingredients</h2></center>
+      <br/>
+      <div className="container products">
+      {igredientsData.map((igredient, index) =>
+      <div className={ingredients.includes(igredientsData[index].name) ? 'greenBG item' : 'whiteBG item'}  key={index} onClick={() => addItem(index)}>
+        <img src={igredient.img} alt={igredient.name}/>
+        <h6>{igredient.name}</h6>
+      </div>
+    
+      )}
+      </div>
+      <center><div className='getRecipesBtn' onClick={() => getRecipes()}><h5>Get Recipes</h5></div></center>
+  
+      <br/>
+      <center><h2>Recipes</h2></center>
+      <br/>
+  
+      
+  
+      <div className="container products">
+      {recipes.map((recipe, index) => <div key={index} className="item-big">
+      {/* <Link to={{path:'/recipe', state:recipe}}> */}
+      <Link to='/recipe' state={recipe}>
+      <img src={recipe.image}></img>
+      </Link>
+      <h6>{recipe.name}</h6>
+      
+      </div>)}
+      </div>
+  
+  
+      </>
+    );
+  }
+
+  const rootElement = document.getElementById('root');
+
+  return(
+
+
+
+    <BrowserRouter>         
+
+
+    <Routes>
+    <Route path="/" element={<RecipePage/>} exact>                    
+    </Route>
+    <Route path="recipe" element={<Recipe/>}></Route>
+    </Routes>
+    
+
+    
+    </BrowserRouter>
+
+
+
+
+
+
+
   );
+
+  
 }
+
+
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+
+function Recipe() {
+    const { state } = this.props;
+    return (
+        <>
+        <h1>TEST</h1>
+        </>
+    );
+}
+
+export default Recipe;
